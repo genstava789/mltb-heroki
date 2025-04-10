18ba7b7d0fe261b28209eb66dc3ebea0cd889500
genstava789 <ryuzakinear154@gmail.com>
2025-04-11 06:54:58 +0700

    Update files_utils.py

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -109,7 +109,7 @@ def exit_clean_up(signal, frame):
     try:
         LOGGER.info("Please wait, while we clean up and stop the running downloads")
         clean_all()
-        srun(["pkill", "-9", "-f", "avaj|c2aira|enolcr|gepmff|gunicorn|sulpdbznbas|xon-tnerrottibq"])
+        srun(["pkill", "-9", "-f", "avaj|c2aira|enolcr|ffmpeg|gunicorn|sulpdbznbas|xon-tnerrottibq"])
         sexit(0)
     except KeyboardInterrupt:
         LOGGER.warning("Force Exiting before the cleanup finishes!")
